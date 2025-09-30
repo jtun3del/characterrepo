095a24b6e3820bc45cf0ced9e59ce827ac5fd644
Jacob Tunnicliff <jtunnicliff@my.wctc.edu>
2025-09-30 13:22:20 -0500

    the same thing again because i forgot to save

---
+++ mario.cs
@@ -0,0 +1,9 @@
+public class Mario : Character
+{
+    public List<string> Alias { get; set; } = [];
+
+    public override string Display()
+    {
+        return base.Display() + $"Alias : {string.Join(", ", Alias)}\n";
+    }
+}
